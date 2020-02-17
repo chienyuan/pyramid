0d4096b6ff026b1490f5364182ae0813a8e37927
Eric Feng <chienyuan.feng@gmail.com>
2020-02-16 20:27:38 -0600

    feat: working version of minimax

--- pyramid.go
+++ pyramid.go
@@ -1,390 +1,432 @@
 package main
 
 import (
-  "fmt"
-  "bufio"
-  "os"
-  "math"
-  "math/rand"
-  "sort"
-  //"github.com/goml/gobrain"
-  //"strings"
+	"bufio"
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"sort"
+	//"github.com/goml/gobrain"
 )
 
-
 type node struct {
-  step      uint
-  bestchild uint
-  children  uint
-  cnum      int
-  value     int
+	step      uint
+	bestchild uint
+	children  uint
+	cnum      int
+	value     int
 }
 
 type game struct {
-  steps[] int
+	steps []int
 }
 
-// token val 
-// start = 000000000000000 
+// start = 000000000000000
 // 0 : free
 // 1 : non-free
 type board struct {
-  val int
+	state int
 }
 
 type pyramid struct {
-  board board
-  root,temp,node_buff  node
-  win[6299] uint
-  //valid_moves[] int
+	board board
+	level int
+	win   [6299]uint
+	//validMoves[] int
 }
 
-//     val= 111000000000
-//  pos 2 = 010000000000 and
-//    ret   010000000000 
+// Checking the state of  position
+// state = 111000000000
+// pos 2 = 010000000000 and
+//   ret   010000000000
 // so if pos == ret , return 1
-func (b board) get(pos int) int {
-  v := int(math.Pow(2,float64(pos)))
-  if ( v & b.val )  == v {
-    return 1
-  }
-  return 0
+func (b board) isTaken(pos int) bool {
+	v := int(math.Pow(2, float64(pos)))
+	if (v & b.state) == v {
+		return true
+	}
+	return false
+}
+
+func (b board) isFree(pos int) bool {
+	v := int(math.Pow(2, float64(pos)))
+	if (v & b.state) == v {
+		return false
+	}
+	return true
 }
 
 func (b *board) set(pos int) {
-  v := int(math.Pow(2,float64(pos)))
-  b.val |= v
+	v := int(math.Pow(2, float64(pos)))
+	b.state |= v
 }
 
 func (b *board) unset(pos int) {
-  v := int(math.Pow(2,float64(pos)))
-  b.val ^= v 
+	v := int(math.Pow(2, float64(pos)))
+	b.state ^= v
 }
 
-func (b *board) addvals(move int) {
-  b.val += move
+func (b *board) placeMove(move int) {
+	b.state += move
 }
 
-func (b board) vals() int {
-  return b.val
+func (b *board) removeMove(move int) {
+	b.state -= move
+}
+
+func (b board) getState() int {
+	return b.state
 }
 
 // after play pos , it only got one token left?
-func (b board) last_token(pos int) bool {
-  b.set(pos)
-  var last bool
-  for i:= 0 ; i < 15 ; i++ {
-    if b.val == int(math.Pow(2,float64(i))) {
-      last = true
-      break
-    }
-  }
-  b.unset(pos)
-  return last
+func (b board) isLastMove(move int) bool {
+	b.placeMove(move)
+	var last bool = false
+
+	if b.state == 32767 {
+		last = true
+	}
+	b.removeMove(move)
+	return last
 }
 
 func (b *board) reset() {
-  b.val = 0
+	b.state = 0
 }
 
-
 //  0(0,4)
-//  1(1,3) 2(1,5) 
+//  1(1,3) 2(1,5)
 //  3(2,2) 4(2,4) 5(2,6)
 //  6(3,1) 7(3,3) 8(3,5) 9(3,7)
 //  A(4,0) B(4,2) C(4,4) D(4,6) E(4,8)
-func (py pyramid ) display(){
-  // alias
-  pl := fmt.Println
-  p := fmt.Print
-  d := 5  // depth 
-  t :=  0 // start token value 
-  for i:= 0 ; i < d  ; i++ {
-    // print prefix space
-    for k := 1 ; k < d - i ; k++ {
-      p(" ")
-    }
+func (py pyramid) display() {
+	d := 5 // depth
+	t := 0 // start token value
+	for i := 0; i < d; i++ {
+		// print prefix space
+		for k := 1; k < d-i; k++ {
+			fmt.Print(" ")
+		}
 
-    // print token
-    for j :=0 ; j <  i+1 ; j ++ {
-      if py.board.get(t) == 0 {
-        p(string(t+65))
-      } else {
-        p("*")
-      }
-      p(" ")
-      t++;
-    }
+		// print token
+		for j := 0; j < i+1; j++ {
+			if py.board.isTaken(t) {
+				fmt.Print("*")
+			} else {
+				fmt.Print(string(t + 65))
+			}
+			fmt.Print(" ")
+			t++
+		}
 
-    // print endl
-    pl("")
-  }
-  pl( py.board.vals())
+		// print endl
+		fmt.Println("")
+	}
+	fmt.Println(py.board.getState())
 }
 
-func (py *pyramid ) human_move()  int{
-  pl := fmt.Println
-  p  := fmt.Print
-  var valid_moves [] int = py.board.gen_valid_moves()
-  p("Please enter your move (A-N)? ")
-  reader := bufio.NewReader(os.Stdin)
-  var str string
-  var move int
-  for {
-    str,_ = reader.ReadString('\n')
-    str = str[:len(str)-1]
+func (py *pyramid) humanMove() int {
+	p := fmt.Print
+	var validMoves []int = py.board.getValidMoves()
+	p("Please enter your move (A-N)? ")
+	reader := bufio.NewReader(os.Stdin)
+	var str string
+	var move int
+	for {
+		str, _ = reader.ReadString('\n')
+		str = str[:len(str)-1]
 
-    pl("len(str)=",len(str))
-    // only allow pick 1,2,3 token
-    var valid bool
-    move,valid =  is_valid_move(str,valid_moves)
-    if valid {
-      break
-    }
+		var valid bool
+		move, valid = isValidMove(str, validMoves)
+		if valid {
+			break
+		}
 
-    p("Invalid !!! Please enter your move again (A-N)? ")
-  }
+		p("Invalid !!! Please enter your move again (A-N)? ")
+	}
 
-  for _, r := range str {
-    py.board.set( int(r - 65) )
-  }
+	for _, r := range str {
+		py.board.set(int(r - 65))
+	}
 
-  py.display()
+	py.display()
 
-  return move
+	return move
 }
 
-func (py *pyramid) minimax(pos int) int {
-  if py.board.last_token(pos) {
-    return 1
-  } else {
-    return 0
-  }
-
-
-
+func max(v1 int, v2 int) int {
+	if v2 >= v1 {
+		return v2
+	} else {
+		return v1
+	}
+}
+func min(v1 int, v2 int) int {
+	if v2 <= v1 {
+		return v2
+	} else {
+		return v1
+	}
 }
 
-func (py *pyramid) compute_move(play bool) int{
-  p := fmt.Println
-  var valid_moves [] int = py.board.gen_valid_moves()
+func (b board) minimax(move int, depth int, maxPlay bool) int {
+	last := b.isLastMove(move)
 
-  
+	if last {
+		if maxPlay {
+			return 1
+		}
+		return -1
+	}
 
-  // TODO: add AI logic here
-  var pick int = -1
-  for i,pos := range valid_moves {
-    if py.minimax(pos) == 1 {
-      pick = i
-      p("winning pick:" , pick)
-      break
-    }
-  } 
+	if depth == 0 {
+		return 0
+	}
 
-  // random move
-  if pick == -1 {
-    p("random pick:" , pick)
-    pick = rand.Intn(len(valid_moves))
-  }
+	b.placeMove(move)
+	var validMoves []int = b.getValidMoves()
 
-  var move int = valid_moves[pick]
-
-  if play {
-    p("len(valid_moves)",len(valid_moves))
-    p("my move valid_moves[", pick ,"]=",move)
-  }
-  py.board.addvals(move)
-  py.display()
-  return move
+	var value int = 0
+	if maxPlay {
+		value = -1
+		for _, child := range validMoves {
+			v := b.minimax(child, depth-1, false)
+			value = max(value, v)
+		}
+	} else {
+		value = 1
+		for _, child := range validMoves {
+			v := b.minimax(child, depth-1, true)
+			value = min(value, v)
+		}
+	}
+	b.removeMove(move)
+	return value
 }
 
-func is_valid_move ( str string, valid_moves []int) ( int,bool ) {
-    if len(str) < 0 || len(str) > 3 { 
-      return 0,false
-    }
+func (py *pyramid) computeMove(play bool) int {
+	var cb board
+	cb.state = py.board.getState()
 
-    var move int
-    // conver string to move value
-    for _, r := range str {
-      move += int( math.Pow(2,float64(r - 65) ) )
-    }
-    fmt.Println("move=",move)
+	var validMoves []int = cb.getValidMoves()
+	validLen := len(validMoves)
+	fmt.Print("len=", validLen, ":")
 
-    // check if the value in valid_moves
-    for _,r := range valid_moves {
-      if move == r {
-        return move ,true
-      }
-    }
+	// TODO: add AI logic here
+	var pick int = -1
+	for i, move := range validMoves {
+		fmt.Print(".")
+		r := cb.minimax(move, py.level, false)
+		if r > 0 {
+			pick = i
+			fmt.Println("winning pick:", pick, " validMoves[]=", validMoves, " break ")
+			break
+		}
+	}
 
-  return move,false
+	// random move
+	if pick == -1 {
+		pick = rand.Intn(len(validMoves))
+		fmt.Println("random  pick:", pick, " validMoves[]=", validMoves)
+	}
+
+	var move int = validMoves[pick]
+
+	if play {
+		//p("len(validMoves)", len(validMoves))
+		//p("my move validMoves[", pick, "]=", move)
+	}
+	py.board.placeMove(move)
+	py.display()
+	return move
 }
 
-func (b board) gen_valid_moves() []int{
-  //    0
-  //   1 2
-  //  3 4 5
-  // 6 7 8 9
-  //a b c d e
-  // two token move data
-  var arr[63] int
-	var temp2 = [30][2]  int {
-	{0,1},{1,3},{3,6},{6,10},{2,4},{4,7},{7,11},{5,8},{8,12},{9,13},
-	{0,2},{2,5},{5,9},{9,14},{1,4},{4,8},{8,13},{3,7},{7,12},{6,11},
-	{1,2},{3,4},{4,5},{6,7},{7,8},{8,9},{10,11},{11,12},{12,13},{13,14}}
+func isValidMove(str string, validMoves []int) (int, bool) {
+	if len(str) < 0 || len(str) > 3 {
+		return 0, false
+	}
+
+	var move int
+	// convert string to integer move value
+	for _, r := range str {
+		move += int(math.Pow(2, float64(r-65)))
+	}
+
+	// check if the value in validMoves
+	for _, r := range validMoves {
+		if move == r {
+			return move, true
+		}
+	}
+
+	return move, false
+}
+
+func (b board) getValidMoves() []int {
+	//    0
+	//   1 2
+	//  3 4 5
+	// 6 7 8 9
+	//a b c d e
+	// two token move data
+	var arr [63]int
+	var temp2 = [30][2]int{
+		{0, 1}, {1, 3}, {3, 6}, {6, 10}, {2, 4}, {4, 7}, {7, 11}, {5, 8}, {8, 12}, {9, 13},
+		{0, 2}, {2, 5}, {5, 9}, {9, 14}, {1, 4}, {4, 8}, {8, 13}, {3, 7}, {7, 12}, {6, 11},
+		{1, 2}, {3, 4}, {4, 5}, {6, 7}, {7, 8}, {8, 9}, {10, 11}, {11, 12}, {12, 13}, {13, 14}}
 
 	// three token move data
-	var temp3 = [18][3] int {
-		{0,1,3},{1,3,6},{3,6,10},{2,4,7},{4,7,11},{5,8,12},
-		{0,2,5},{2,5,9},{5,9,14},{1,4,8},{4,8,13},{3,7,12},
-		{3,4,5},{6,7,8},{7,8,9},{10,11,12},{11,12,13},{12,13,14}}
+	var temp3 = [18][3]int{
+		{0, 1, 3}, {1, 3, 6}, {3, 6, 10}, {2, 4, 7}, {4, 7, 11}, {5, 8, 12},
+		{0, 2, 5}, {2, 5, 9}, {5, 9, 14}, {1, 4, 8}, {4, 8, 13}, {3, 7, 12},
+		{3, 4, 5}, {6, 7, 8}, {7, 8, 9}, {10, 11, 12}, {11, 12, 13}, {12, 13, 14}}
 
 	// one token move
-  var i = 0
-  var j = 0
-  for i = 0; i < 15; i++ {
-    //  need to skip already set token
-    if b.get(i) == 0 {
-      arr[j] = int(math.Pow(2,float64(i)) )
-      j ++
-    }
-  }
+	var i = 0
+	var j = 0
+	for i = 0; i < 15; i++ {
+		//  need to skip already set token
+		if b.isFree(i) {
+			arr[j] = int(math.Pow(2, float64(i)))
+			j++
+		}
+	}
 
-  // two token move
-  for _, r := range temp2 {
-    if b.get(r[0]) == 0 && b.get(r[1]) == 0  {
-      arr[j] = int(math.Pow(2,float64(r[0])) + math.Pow(2,float64(r[1])))
-      j = j + 1
-    }
-  }
+	// two token move
+	for _, r := range temp2 {
+		if b.isFree(r[0]) && b.isFree(r[1]) {
+			arr[j] = int(math.Pow(2, float64(r[0])) + math.Pow(2, float64(r[1])))
+			j = j + 1
+		}
+	}
 
-  // three token move
-  for _, r := range temp3 {
-    if b.get(r[0]) == 0 && b.get(r[1]) == 0 && b.get(r[2]) == 0 {
-      arr[j] = int(math.Pow(2,float64(r[0])) +
-                            math.Pow(2,float64(r[1])) +
-                            math.Pow(2,float64(r[2])))
-      j = j + 1
-    }
-  }
-  // slice the array
-  valid_arr := arr[:j]
-  sort.Ints(valid_arr)
-  var valid_moves[]int = valid_arr
-  fmt.Println("valid_moves=",valid_moves)
-  return valid_moves
+	// three token move
+	for _, r := range temp3 {
+		if b.isFree(r[0]) && b.isFree(r[1]) && b.isFree(r[2]) {
+			arr[j] = int(math.Pow(2, float64(r[0])) +
+				math.Pow(2, float64(r[1])) +
+				math.Pow(2, float64(r[2])))
+			j = j + 1
+		}
+	}
+	// slice the array so it could sort
+	validArr := arr[:j]
+	sort.Ints(validArr)
+	var validMoves []int = validArr
+	return validMoves
 }
 
-
-
-func (py pyramid) is_gameover() bool {
-  for i:=0 ; i < 15 ; i++ {
-    if py.board.get(i) == 0 {
-      return false
-    }
-  }
-  return true
+func (py pyramid) isGameOver() bool {
+	for i := 0; i < 15; i++ {
+		if py.board.isFree(i) {
+			return false
+		}
+	}
+	return true
 }
-
 
 // TODO: need to add game state, step , win strut
-func gen() (map[int]int,bool) {
+func gen() (map[int]int, bool) {
 
-  steps := make(map[int]int)
-  py := pyramid{}
-  s := 0
-  win := false
+	steps := make(map[int]int)
+	py := pyramid{level: 3}
+	s := 0
+	win := false
 
-  for !py.is_gameover() {
-    steps[s]= py.compute_move(false);
-    s++
-    if py.is_gameover() {
-      win = true
-     } else {
-      steps[s]=py.compute_move(false)
-      s++
-      if py.is_gameover() {
-        win = false
-      }
-    }
-  }
-  return steps,win
+	for !py.isGameOver() {
+		steps[s] = py.computeMove(false)
+		s++
+		if py.isGameOver() {
+			win = true
+		} else {
+			steps[s] = py.computeMove(false)
+			s++
+			if py.isGameOver() {
+				win = false
+			}
+		}
+	}
+	return steps, win
 }
 
-func main(){
+func main() {
+	var train bool
 
-  var train bool 
-  if train {
-    pl := fmt.Println
-   games := make(map[int] map[int]int)
-   var win bool
-   for i:=0 ; i < 100 ; i++ {
-     games[i],win = gen()
-   }
-   pl("games: win=",win," steps",games)
-  }
+	if train {
+		games := make(map[int]map[int]int)
+		var win bool
+		for i := 0; i < 100; i++ {
+			games[i], win = gen()
+		}
+		fmt.Println("games: win=", win, " steps", games)
+	}
 
-  play()
+	play()
 }
 
-func play(){
-  pl := fmt.Println
+func play() {
+	pl := fmt.Println
+	p := fmt.Print
 
-  py := pyramid{}
+	py := pyramid{level: 3}
 
 	pl("Welcome to Pyramid game ")
 	pl("The Rules of Pyramid: Players alternate take out one to three ")
 	pl("tokens in one line. Who takes the last one token is loser")
 
-  for {
-    py.board.reset()
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Do you want to move first(y/n)?")
-    ans,_ := reader.ReadByte()
-    _,_ = reader.ReadByte()
-    s     := 0
-    steps := make(map[int]int)
+	for {
+		py.board.reset()
+		// For Testing
+		//py.board.state = 0B0111111111110000
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Do you want to move first(y/n)?")
+		ans, _ := reader.ReadByte()
+		_, _ = reader.ReadByte()
+		s := 0
+		steps := make(map[int]int)
 
-    if ans == 'y'  {
-      py.display()
-      pl("OK ! You move first.")
-      for !py.is_gameover() {
-        steps[s]= py.human_move();
-        s++
-        if py.is_gameover() {
-          pl("You lose,I win!!!")
-        } else {
-          steps[s]=py.compute_move(true)
-          s++
-          if py.is_gameover() {
-            pl("You win")
-          }
-        }
-      }
-    } else {
-      pl(" OK ! I  move first.")
-      for !py.is_gameover() {
-        steps[s]=py.compute_move(true)
-        s++
-        if py.is_gameover() {
-          pl("You win")
-        } else  {
-          steps[s]=py.human_move()
-          s++
-          if py.is_gameover() {
-            pl("You lose,I win!!")
-          }
-        }
-      }
-    }
+		if ans == 'y' {
+			py.display()
+			pl("OK ! You move first.")
+			for !py.isGameOver() {
+				steps[s] = py.humanMove()
+				s++
+				if py.isGameOver() {
+					pl("You lose,I win!!!")
+				} else {
+					steps[s] = py.computeMove(true)
+					s++
+					if py.isGameOver() {
+						pl("You win")
+					}
+				}
+			}
+		} else {
+			pl(" OK ! I  move first.")
+			for !py.isGameOver() {
+				steps[s] = py.computeMove(true)
+				s++
+				if py.isGameOver() {
+					pl("You win")
+				} else {
+					steps[s] = py.humanMove()
+					s++
+					if py.isGameOver() {
+						pl("You lose,I win!!")
+					}
+				}
+			}
+		}
 
-    pl("Game Over")
-    pl("Game Result:", steps)
-    pl("Do you want to play again(y/n)?")
-    ans,_ = reader.ReadByte()
-    _,_ = reader.ReadByte()
+		pl("Game Over")
+		pl("Game Result:", steps)
+		p("Do you want to play again(y/n)?")
+		ans, _ = reader.ReadByte()
+		_, _ = reader.ReadByte()
 
-    if  ans == 'n'   { break }
-  }
-
+		if ans == 'n' {
+			break
+		}
+	}
 }
